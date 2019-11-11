6375108e43e5c3a2be334c77f4dbe28d46551342
GabrielCambe <almeidasalesgabriel@gmail.com>
2019-11-11 19:13:55 -0300

    avançado

--- ci1164-engperf/medir-perf.sh
+++ ci1164-engperf/medir-perf.sh
@@ -3,36 +3,37 @@
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
 #for i in 32 50 64 100 128 200 256 300 400 512 1000 1024 2000 2048 3000 4000 4096 5000 10000; do
-for i in 32; do
-    for g in L3 L2CACHE FLOPS_DP FLOPS_AVX ; do
-	echo "n = ${i}; grupo = ${g}"
-	likwid-perfctr -O --output $g$i.csv -C 3 -g $g -m ./matmult -n $i
-
-	if [[ $g = "L3" ]]; then
-	    grep -E 'multMatPtrVet|L3 bandwidth' $g$i.csv
-	    grep -E 'multMatRowVet|L3 bandwidth' $g$i.csv
-	    grep -E 'multMatColVet|L3 bandwidth' $g$i.csv
-	fi
-	
-	if [[ $g = "L2CACHE" ]]; then
-	    grep -E 'multMatPtrVet|L2 miss ratio' $g$i.csv
-	    grep -E 'multMatRowVet|L2 miss ratio' $g$i.csv
-	    grep -E 'multMatColVet|L2 miss ratio' $g$i.csv
-	fi
-	
-	if [[ $g = "FLOPS_DP" ]]; then
-	    grep -E 'multMatPtrVet|DP MFLOP/s' $g$i.csv
-	    grep -E 'multMatRowVet|DP MFLOP/s' $g$i.csv
-	    grep -E 'multMatColVet|DP MFLOP/s' $g$i.csv
-	fi
-
-	if [[ $g = "FLOPS_AVX" ]]; then
-	    grep -E 'multMatPtrVet|AVX DP MFLOP/s' $g$i.csv
-	    grep -E 'multMatRowVet|AVX DP MFLOP/s' $g$i.csv
-	    grep -E 'multMatColVet|AVX DP MFLOP/s' $g$i.csv
-	fi
+#for g in L3 L2CACHE FLOPS_DP FLOPS_AVX; do
+for g in L3; do
+    #echo "n = ${i}; grupo = ${g}"
+    #likwid-perfctr -O --output $g$i.csv -C 3 -g $g -m ./matmult -n $i
+    likwid-perfctr -O -C 3 -g $g -m ./matmult -n $1
+    #likwid-perfctr -C 3 -g $g -m ./matmult -n $i
     
-    done
+    #if [[ $g = "L3" ]]; then
+    #    for i in multMatPtrVet multMatRowVet multMatColVet; do 
+    #	grep -E '$i|L3 bandwidth' $g$i.csv
+    #    done
+    #fi
+    
+    #if [[ $g = "L2CACHE" ]]; then
+    #    grep -E 'multMatPtrVet|L2 miss ratio' $g$i.csv
+    #    grep -E 'multMatRowVet|L2 miss ratio' $g$i.csv
+    #    grep -E 'multMatColVet|L2 miss ratio' $g$i.csv
+    #fi
+    
+    #if [[ $g = "FLOPS_DP" ]]; then
+    #    grep -E 'multMatPtrVet|DP MFLOP/s' $g$i.csv
+    #    grep -E 'multMatRowVet|DP MFLOP/s' $g$i.csv
+    #    grep -E 'multMatColVet|DP MFLOP/s' $g$i.csv
+    #fi
+    
+    #if [[ $g = "FLOPS_AVX" ]]; then
+    #    grep -E 'multMatPtrVet|AVX DP MFLOP/s' $g$i.csv
+    #    grep -E 'multMatRowVet|AVX DP MFLOP/s' $g$i.csv
+    #    grep -E 'multMatColVet|AVX DP MFLOP/s' $g$i.csv
+    #fi
+    
 done 
 
 echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
---
+++ ci1164-engperf/medir-perfL3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
+rm *.dat
+
+for g in L3; do
+    for n in 32 50 64 100 128 200 256 300 400 512 1000 1024 2000 2048 3000 4000 4096 5000 10000; do
+	likwid-perfctr -O -C 3 -g $g -m ./matmult -n $n > debug.log
+	raw_data=$(cat debug.log);
+	for region in multMatPtrVet; do 
+	    metrica=$(echo $raw_data | awk -F'[:,;]' '{ for(i=1; i<=NF; i++){ if(($i~"TABLE")&&($(i+1)~"Region multMatPtrVet")&&($(i+2)~"Group 1 Metric")&&($(i+3)~"L3")){ for(j=i+1; j<=NF; j++){ if($j~"TABLE"){ exit; } else if($j~/L3 bandwidth \[MBytes\/s\]/){ print $(j+1); }}}}}')
+	    echo $n $metrica >> $g$region.dat
+	done
+
+	for region in multMatRowVet; do 
+	    metrica=$(echo $raw_data | awk -F'[:,;]' '{ for(i=1; i<=NF; i++){ if(($i~"TABLE")&&($(i+1)~"Region multMatRowVet")&&($(i+2)~"Group 1 Metric")&&($(i+3)~"L3")){ for(j=i+1; j<=NF; j++){ if($j~"TABLE"){ exit; } else if($j~/L3 bandwidth \[MBytes\/s\]/){ print $(j+1); }}}}}')
+	    echo $n $metrica >> $g$region.dat
+	done
+
+	for region in multMatColVet; do 
+	    metrica=$(echo $raw_data | awk -F'[:,;]' '{ for(i=1; i<=NF; i++){ if(($i~"TABLE")&&($(i+1)~"Region multMatColVet")&&($(i+2)~"Group 1 Metric")&&($(i+3)~"L3")){ for(j=i+1; j<=NF; j++){ if($j~"TABLE"){ exit; } else if($j~/L3 bandwidth \[MBytes\/s\]/){ print $(j+1); }}}}}')
+	    echo $n $metrica >> $g$region.dat
+	done
+    done
+done
+
+echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
+
+
+
+
+
+#./medir-perf.sh | while read line || [[ -n $line ]]; do
+    #echo $line |  awk -F'[:,;]' '{{for (i=1; i<=NF; i++) if ($i~/Table/) $table += $i; i++; while($i!~/Table/) $table += $i; i++} print $table }'
+    #echo $line |  awk -F'[:,;]' '{ for(i=1; i<=NF; i++){ if(($i~/TABLE/)&&($(i+1)~/Region multMatPtrVet/)&&($(i+2)~/Group 1 Metric/)&&($(i+3)~/L3/)){ print $i; for(j=i+1; j<=i+1; j++){ teste=$teste $j; print $j } print $teste }}}'
+#done
+
+#./medir-perf.sh | while read line || [[ -n $line ]]; do
+#    #echo $line |  awk -F'[:,;]' '{for (i =1; i<=NF ; i++) {print $i}}'
+#    echo $line |  awk -F'[:,;]' '{ if({for (i=1; i<=NF; i++) {if $i~/Group 1 Metric/}}'
+#done
+
+#region=multMatPtrVet
